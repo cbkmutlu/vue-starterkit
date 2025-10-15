a36485092ad9448ee92e4572ed0af45f41616666
cbkmutlu <cbkmutlu@gmail.com>
2025-10-15 23:26:25 +0300

    feat(avatar): add fallback avatar component
    - Display default avatar if image fails
    - Added image prop with default value

---
+++ src/components/Avatar/FallbackAvatar.vue
@@ -0,0 +1,35 @@
+<template>
+   <v-avatar
+      v-if="props.image"
+      rounded
+      size="28">
+      <v-img
+         v-bind:src="getMedia(props.image || 'no-image.png')"
+         cover>
+         <template v-slot:error>
+            <div class="flex-center h-full">
+               <v-icon
+                  icon="$photo"
+                  size="default" />
+            </div>
+         </template>
+      </v-img>
+   </v-avatar>
+   <v-avatar
+      v-else
+      rounded
+      size="28">
+      <v-icon
+         icon="$photo"
+         size="default" />
+   </v-avatar>
+</template>
+
+<script lang="ts" setup>
+import type { TAvatar } from "@/utils/types";
+
+// states
+const props = withDefaults(defineProps<TAvatar & { image: any }>(), {
+   image: "no-image.png"
+});
+</script>
