1a442c8578db1a35ce9acee09ff2d5717bf38fec
cbkmutlu <cbkmutlu@gmail.com>
2025-10-28 09:41:28 +0300

    feat: add info notify component
    - Created reusable info notify component
    - Based on Vuetify v-alert component

---
+++ src/components/Notify/InfoNotify.vue
@@ -0,0 +1,36 @@
+<template>
+   <v-alert
+      v-bind:closable="props.closable"
+      v-bind:icon-size="'22px'"
+      type="info"
+      density="comfortable"
+      variant="tonal">
+      <div class="text-xs flex items-center">
+         <slot />
+      </div>
+
+      <template
+         v-if="props.closable"
+         v-slot:close="{ props: slotProps }">
+         <v-btn
+            density="compact"
+            icon="$close"
+            v-ripple
+            size="default"
+            variant="text"
+            @click="slotProps.onClick()" />
+      </template>
+   </v-alert>
+</template>
+
+<script lang="ts" setup>
+import type { TAlert } from "@/utils/types";
+type TProps = {
+   closable?: boolean;
+};
+
+// states
+const props = withDefaults(defineProps<TAlert & TProps>(), {
+   closable: true
+});
+</script>
