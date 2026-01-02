01192a619ea0547aae7dbdb376d6cc23bdf50ecb
cbkmutlu <cbkmutlu@gmail.com>
2026-01-02 17:22:30 +0300

    perf(bootstrap): parallelize app initialization
    - Run locale, menu, and route loads concurrently
    - Use Promise.all for faster data fetching

--- src/main.ts
+++ src/main.ts
@@ -3,48 +3,45 @@ import "@/assets/style/main.scss";
 // #rest
 import App from "@/App.vue";
 import { registerDirective } from "@/plugins/directive";
-import { registerProvider } from "@/plugins/provider";
 import { i18n } from "@/plugins/i18n";
 import { pinia } from "@/plugins/pinia";
+import { registerProvider } from "@/plugins/provider";
 import { options, query } from "@/plugins/query";
 import { router } from "@/plugins/router";
 import { vuetify } from "@/plugins/vuetify";
 
 const app = createApp(App);
+
 app.use(pinia);
 app.use(i18n);
 app.use(vuetify);
 app.use(query, options);
 
-const appStore = useAppStore();
-
-// route
-await loadRoute().then((routes) => {
-   routes.forEach((route) => {
-      router.addRoute(route);
-   });
-});
-
-// locale
-appStore.setLocaleLoading(true);
-await loadLocale(getLocale()).then((messages) => {
-   setLocale(i18n, getLocale(), messages);
-});
-appStore.setLocaleLoading(false);
-
-// menu
-appStore.setMenuLoading(true);
-await loadMenu().then((menu) => {
-   appStore.setMenu({ ...menu });
-});
-appStore.setMenuLoading(false);
-
-// register
 registerProvider(app);
 registerDirective(app);
 
-// mount
-app.use(router);
-router.isReady().then(() => {
-   app.mount("#app");
+(async () => {
+   const appStore = useAppStore();
+
+   appStore.setLocaleLoading(true);
+   appStore.setMenuLoading(true);
+
+   const [messages, menu, routes] = await Promise.all([loadLocale(getLocale()), loadMenu(), loadRoute()]);
+
+   setLocale(i18n.global, getLocale(), messages);
+   appStore.setMenu(menu);
+
+   routes.forEach((route) => {
+      router.addRoute(appConfig.router.name, route);
+   });
+
+   appStore.setLocaleLoading(false);
+   appStore.setMenuLoading(false);
+
+   app.use(router);
+   router.isReady().then(() => {
+      app.mount("#app");
+   });
+})().catch((error) => {
+   console.error("Bootstrap failed", error);
 });
