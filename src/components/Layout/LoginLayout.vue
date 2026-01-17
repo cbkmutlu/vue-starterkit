ef328c4025a7a1196a239c8a252d482c03521bd5
cbkmutlu <cbkmutlu@gmail.com>
2026-01-17 18:16:40 +0300

    refactor(layout): improve login layout responsiveness
    - Replaced v-sheet with v-container
    - Adjusted column widths for responsiveness

--- src/components/Layout/LoginLayout.vue
+++ src/components/Layout/LoginLayout.vue
@@ -1,16 +1,14 @@
 <template>
-   <v-sheet
-      class="flex h-full items-center justify-center bg-white bg-[url('/src/assets/image/login-bg2.jpg')] bg-cover bg-right bg-no-repeat"
-      tile>
+   <v-container class="flex h-full max-w-full items-center justify-center bg-white bg-[url('/src/assets/image/login-bg2.jpg')] bg-cover bg-right bg-no-repeat">
       <v-row>
          <v-col
             lg="4"
-            md="5"
+            md="6"
             offset-sm="1"
-            sm="6"
-            xl="3">
+            sm="8"
+            xl="2">
             <router-view />
          </v-col>
       </v-row>
-   </v-sheet>
+   </v-container>
 </template>
